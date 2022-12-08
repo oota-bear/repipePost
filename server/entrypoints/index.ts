1a76f5ca8f1b3794c8374e3c4fa929e817251a29
shioriota <shiori.ota@hrotti.co.jp>
2022-12-08 19:14:41 +0900

    prisma修正

--- server/entrypoints/index.ts
+++ server/entrypoints/index.ts
@@ -1,5 +1,8 @@
 import { init } from '$/service/app'
 import { API_SERVER_PORT } from '$/service/envValues'
+import { PrismaClient } from '@prisma/client'
 
 const app = init()
 app.listen(API_SERVER_PORT)
+
+
---
+++ server/service/recipeTitle.ts
@@ -0,0 +1,3 @@
+import { RecipeTitle} from "@prisma/client";
+
+
